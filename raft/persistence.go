a361a15e5266b2b52d993a7ac4e9fe0b2c4b2c2d
Aditya Bhalerao <adityab7968@gmail.com>
2025-10-10 20:30:20 +0530

    Add persist and restore for Raft state

---
+++ raft/persistence.go
@@ -0,0 +1,42 @@
+package raft
+
+import (
+	"log"
+
+	"github.com/arbhalerao/raft/storage"
+)
+
+func (rf *Rf) persist() {
+	entries := make([]storage.LogEntry, len(rf.lg))
+	for i, e := range rf.lg {
+		entries[i] = storage.LogEntry{
+			Term:  e.Term,
+			Index: e.Idx,
+			Cmd:   e.Cmd,
+		}
+	}
+	p := &storage.Persistent{
+		CurrentTerm: rf.ct,
+		VotedFor:    rf.vf,
+		Log:         entries,
+	}
+	if err := rf.store.Save(p); err != nil {
+		log.Printf("[node %d] persist error: %v", rf.id, err)
+	}
+}
+
+func (rf *Rf) restore() error {
+	p, err := rf.store.Load()
+	if err != nil {
+		return err
+	}
+	rf.ct = p.CurrentTerm
+	rf.vf = p.VotedFor
+	if len(p.Log) > 0 {
+		rf.lg = make([]Entry, len(p.Log))
+		for i, e := range p.Log {
+			rf.lg[i] = Entry{Term: e.Term, Idx: e.Index, Cmd: e.Cmd}
+		}
+	}
+	return nil
+}
